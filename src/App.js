5ef7507aa5dd4013f48240c24a54c644d3d939ea
Tuan Anh <manhhungit.37@gmail.com>
2021-08-10 17:05:11 +0700

    pagination

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [perPage, setPerPage] = useState(3);
+  const [currentPage, setCurrentPage] = useState(1);
+  const indexLastTodo = currentPage * perPage;
+  const indexFirstTodo = indexLastTodo - perPage;
+  const currentTodos = todos.slice(indexFirstTodo, indexLastTodo);
+  const pageNumbers = [];
+
+  useEffect(() => {
+    async function getTodos() {
+      const res = await axios.get("https://tony-json-server.herokuapp.com/api/todos")
+      const { data } = res.data;
+      setTodos(data);
+    }
+    getTodos();
+    
+  }, [])
+
+  for (var i = 1; i <= Math.ceil(todos.length / perPage); i++) {
+    pageNumbers.push(i);
+  }
+
+  console.log(todos)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {currentTodos.length > 0 && currentTodos.map(todo => <div key={todo.id}>{todo.title}  </div>)}
+      <ul>
+        {pageNumbers.length > 0 && pageNumbers.map(num => (
+          <li key={num}>
+            <a href="/" onClick={() => setCurrentPage(num)}>
+              {num}
+            </a>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
